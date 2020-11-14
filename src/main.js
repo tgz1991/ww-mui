b5500ccd3d88069b8bada83445992863a1e5866d
seven2020 <7422840+seven2010@user.noreply.gitee.com>
2020-11-14 14:55:03 +0800

    vant 修改

--- src/main.js
+++ src/main.js
@@ -2,20 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
 import App from './App'
-import Home from './components/HelloFromVux'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import router from './router'
 
+Vue.use(Vant);
 Vue.use(VueRouter)
-
-const routes = [{
-  path: '/',
-  component: Home
-}]
-
-const router = new VueRouter({
-  routes
-})
 
 FastClick.attach(document.body)
 
